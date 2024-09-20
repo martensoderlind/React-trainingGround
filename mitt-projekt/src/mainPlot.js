4232083da68312bc02d7a351259bbdde52ed3228
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-09-20 14:13:18 +0200

    fixed header style

--- mitt-projekt/src/mainPlot.js
+++ mitt-projekt/src/mainPlot.js
@@ -15,7 +15,7 @@ const data = [
 
 const TrainingGraph =()=>{
     return(
-        <div style={{width:'80%', height: 300}}>
+        <div style={{width:'100%', height: 300}}>
             <ResponsiveContainer>
                 <LineChart
                 data={data}
