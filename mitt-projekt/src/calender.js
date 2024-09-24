ebba78c3be948f72077334f4ccb54164ba70398f
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-09-24 16:11:39 +0200

    added functionality to change month

--- mitt-projekt/src/calender.js
+++ mitt-projekt/src/calender.js
@@ -5,8 +5,15 @@ const Calender = ()=>{
 
     const daysInMonth = new Date(currentDate.getFullYear(),currentDate.getMonth()+1,0).getDate();
     const firstDayOfMonth = new Date(currentDate.getFullYear(),currentDate.getMonth(),0).getDay();
-    console.log(`dagar i månad: ${daysInMonth}`);
-    console.log(`första dag i månad: ${firstDayOfMonth}`);
+    // console.log(`dagar i månad: ${daysInMonth}`);
+    // console.log(`första dag i månad: ${firstDayOfMonth}`);
+
+    const prevMonth=()=>{
+        setCurrentDate(new Date(currentDate.getFullYear(),currentDate.getMonth()-1,1))
+    };
+    const nextMonth=()=>{
+        setCurrentDate(new Date(currentDate.getFullYear(),currentDate.getMonth()+1,1));
+    };
 
     const renderDays = ()=>{
         const days =[];
@@ -14,7 +21,7 @@ const Calender = ()=>{
             days.push(<div key={i-firstDayOfMonth} className="p-2"></div>)
         };
         for (let i = 1; i < daysInMonth+1; i++) {
-            days.push(<div key={i} className="p-2">{i}</div>)
+            days.push(<div key={i} className="p-2 hover:bg-gray-200">{i}</div>)
         };
         return days;
     };
@@ -22,11 +29,11 @@ const Calender = ()=>{
     return (
         <div className="calenderMain bg-gray-50 rounded-lg ">
             <div className="flex space-x-2 px-2 bg-gray-300 rounded-t-lg">
-                <button className="">föregående</button>
+                <button className="" onClick={prevMonth}>föregående</button>
                 <h2>
                     månad
                 </h2>
-                <button className="">nästa</button>
+                <button className="" onClick={nextMonth}>nästa</button>
             </div>
             <div className="grid grid-cols-7 gap-1 p-4">
                 {['Mån','Tis','Ons','Tors','Fre','Lör','Sön'].map((day)=>(
