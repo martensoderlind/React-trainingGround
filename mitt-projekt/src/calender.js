5c543dda03f2d5c247831145660318a8de58c3e9
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-09-24 12:05:38 +0200

    setting up new component Celender

---
+++ mitt-projekt/src/calender.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+
+const Calender = ()=>{
+
+
+    return (
+        <div className="calenderMain">
+            
+        </div>
+    )
+};
+
+export default Calender;
