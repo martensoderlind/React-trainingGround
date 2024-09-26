af9d48d5510fab49b9f4ecd1dd6fcf98dc1c569b
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-09-26 11:19:59 +0200

    fixed layout of calender header

--- mitt-projekt/src/calender.js
+++ mitt-projekt/src/calender.js
@@ -64,7 +64,7 @@ const Calender = ()=>{
     };
     return (
         <div className="calenderMain bg-gray-50 rounded-lg mx-4 my-2 w-96 h-auto shadow-xl">
-            <div className="flex space-x-16 p-2 bg-gray-300 rounded-t-lg">
+            <div className="flex space-x-16 p-2 bg-gray-300 rounded-t-lg justify-between">
                 <svg onClick={prevMonth} xmlns="http://www.w3.org/2000/svg" width="1.6em" height="1.6em" viewBox="0 0 24 24"><path fill="currentColor" d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6l6 6z"/></svg>
                 <h2 className="font-bold">
                     {renderMonth()}
