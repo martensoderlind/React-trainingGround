94baa2514fdee89cd91d38970b306f7c70d6d14a
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-09-26 13:51:11 +0200

    Set ups fore change of view

--- mitt-projekt/src/App.js
+++ mitt-projekt/src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import MyHeader from './header.js'
 import MainPlot from './mainPlot.js'
 import Calender from './calender.js'
 import Weather from './weather.js'
+import PrevRuns from './runs.js'
 
 
 function App() {
+  const [showPlot,setShowPlot] = useState(true);
+  
+  const toggleView = ()=>{
+    setShowPlot(!showPlot);
+  };
+
   return (
     <div className="App">
-      <MyHeader />
+      <MyHeader view={toggleView}/>
       <div className='flex'>
-        <MainPlot />
+        {showPlot ? <MainPlot />:<PrevRuns />}
         <div>
           <Calender />
           <Weather />          
---
+++ mitt-projekt/src/runs.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import {data} from './data.js'
+
+function PrevRuns(){
+
+    return(
+        <div>
+            test
+        </div>
+    )
+
+};
+
+export default PrevRuns;
