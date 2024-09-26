a574d7c258e7c8397fd04ebcacd158d8b9231ef0
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-09-26 11:07:21 +0200

    updated api call to only be called once

--- mitt-projekt/src/weather.js
+++ mitt-projekt/src/weather.js
@@ -1,8 +1,7 @@
 import React, {useState, useEffect} from 'react';
+import url from './apiKey.js'
 
-async function getWeatherData() {
-    const url = 'http://api.weatherapi.com/v1/current.json?key=ccbcf3b829164884a11150305242509&q=Stockholm&aqi=no';
-
+async function getWeatherData(url) {
     try{
         const response = await fetch(url);
         if(!response.ok){
@@ -12,6 +11,7 @@ async function getWeatherData() {
         const weatherData={
             name: data.location.name,
             temp: data.current.temp_c,
+            feelsLikeTemp: data.current.condition.feelslike_c,
             condition: data.current.condition.text,
             conditionIcon: data.current.condition.icon,
             wind: data.current.wind_kph,
@@ -25,15 +25,15 @@ async function getWeatherData() {
 
 function Weather(){
     const [weather,setWeather] =useState(null);
-
+    
     useEffect(()=>{
         const fetchWeather = async ()=>{
-            const weatherData = await getWeatherData();
+            const weatherData = await getWeatherData(url);
             setWeather(weatherData);
         };
         fetchWeather();
 
-    });
+    },[]);
 
     if(!weather) {
         return <div>Loading...</div>
@@ -41,13 +41,20 @@ function Weather(){
     
     return(
         <div className='flex bg-gray-100 rounded-lg w-96 ml-4 h-48 shadow-xl'>
-            <div className='p-2'>
+            <div className='p-2 w-1/1'>
                 <h1 className=' flex font-semibold text-xl'>{weather.name}</h1>
-                <h2 className=' flex font-semibold'>Temperatur: <p className='font-normal pl-2'> {weather.temp} °C</p></h2>
+                <h1 className='text-xs text-start'>{weather.time}</h1>
+                <div className='flex justify-between'>
+                    <img src={weather.conditionIcon} alt="Weather icon" className=''/>
+                    <div className='pt-3'>
+                        <h1 className=' flex font-semibold text-end'>{weather.temp} °C</h1>
+                        <p className='text-xs pl-2 text-end'>Känns som {weather.feelsLikeTemp} °C</p>
+                    </div>
+                </div>
+                
                 <h2 className=' flex font-semibold'>Väderförhållanden: <p className='font-normal pl-2'> {weather.condition}</p></h2>
                 <h2 className=' flex font-semibold'>Vind: <p className='font-normal pl-2'> {weather.wind} kph</p></h2>
             </div>
-            <img src={weather.conditionIcon} alt="Weather icon" className=''/>
         </div>
     );
 }
